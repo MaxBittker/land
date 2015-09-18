c382c7da681a9895e54eed052a26a19a0340344b
Max Bittker <max@linux-feoj.site>
2015-09-18 00:58:59 -0400

    gtn tmrw

--- land.js
+++ land.js
@@ -9,9 +9,10 @@ var blue = new Color(50, 60, 160);
 var x = 6;
 var y = 6;
 var z = 0;
-var delta = .3;
+var delta = 1;
 
 var dir = 0;
+var c = 0;
 
 
 window.requestAnimFrame = (function() {
@@ -26,50 +27,63 @@ window.requestAnimFrame = (function() {
 
 (function animloop() {
     requestAnimFrame(animloop);
-    render();
+    render(0);
 })();
 
-function render() {
+function render(n) {
+    if (n===30)
+    {
+    	x = 3+ Math.random()*3
+    	y = 3+ Math.random()*3
+    }
     if (oob())
         dir = (dir + 1) % 4;
 
     switch (dir) {
         case 0:
             x += delta;
-            // y += delta/10;
+            // y += delta/30;
             // y += delta;
             break;
         case 1:
             // x -= delta;
-            // x -= delta/10;
+            // x -= delta/30;
             y += delta;
             break;
         case 2:
             x -= delta;
             // y -= delta;
-            // y -= delta/10;
+            // y -= delta/30;
             break;
         case 3:
             // x += delta;
-            // x += delta/10;
+            x += delta/30;
             y -= delta;
             break;
         default:
             alert("badcase")
     }
+    if(oob())
+    	render(n+1)
+
+
     draw();
 }
 
 function oob() {
-    // if (Math.sqrt((x * x) + (y * y)) > 10)
-    if ((Math.abs(x-4)+Math.abs(y-3))>8)
+    // if (Math.sqrt((x * x) + (y * y)) > 9)
+    if ((Math.abs(x-4)+Math.abs(y-3))>8.7)
         return true
     else
         return false
 }
 
 function draw() {
-    iso.add(Shape.Pyramid(new Point(x, y, z)), new Color(160 * Math.random(), 60 * Math.random(), 50 * Math.random()));
+    // iso.add(Shape.Prism(new Point(x,y,z-2), 3, 3, 1.5));
+    // iso.add(Shape.Pyramid(new Point(x, y, z)), new Color(11 , 130 , 50 * Math.random()));
+    // iso.add(Shape.Pyramid(new Point(x, y, z)), new Color(160 * Math.random(), 60 * Math.random(), 50 * Math.random()));
+     iso.add(Shape.Prism(new Point(x, y, z)), new Color(160 * Math.random(), 60 * Math.random(), 50 * Math.random()));
+
 }
 
 window.addEventListener("keydown", function(evt) {
@@ -95,7 +109,7 @@ window.addEventListener("keydown", function(evt) {
     // iso.add(Shape.Prism(new Point(coins[n][0]+.3,coins[n][1]+.3,0),.4,.4,.4),new Color(160,90,50));
 
     // }
-    iso.add(Shape.Pyramid(new Point(x, y, z)), new Color(160 * Math.random(), 60 * Math.random(), 50 * Math.random()));
+    render();
 }, false);
 
 
